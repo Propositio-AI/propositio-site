f8b12f59e8ba36185dd379ac8ef3c95b58903ac3
towa0829 <towa0000829@icloud.com>
2026-04-01 18:47:15 +0900

    feat: sort blogs by published date in descending order

--- app/actions/blog.ts
+++ app/actions/blog.ts
@@ -3,7 +3,7 @@ import { supabase } from "@/lib/supabase";
 
 
 export async function getAllBlogs() {
-  const { data, error } = await supabase.from("blogs").select("*");
+  const { data, error } = await supabase.from("blogs").select("*").order("published_at", { ascending: false });
 
   if(error) {
     throw new Error(error.message);
